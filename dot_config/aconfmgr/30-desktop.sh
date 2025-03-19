dd2065c3775b8bbd9ea1e828ecaae8235218d136
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2025-03-18 23:21:16 -0600

    add xwayland-satellite

--- dot_config/aconfmgr/30-desktop.sh
+++ dot_config/aconfmgr/30-desktop.sh
@@ -6,6 +6,7 @@ AddPackage niri # A scrollable-tiling Wayland compositor
 # Common important Wayland apps.
 AddPackage fuzzel # Application launcher for wlroots based Wayland compositors
 AddPackage waybar # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage xwayland-satellite # Xwayland outside your Wayland
 # TODO: We are missing a lot! Notifications, portals, and other important DE components.
 
 # Applications.
