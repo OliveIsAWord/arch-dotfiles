8e47f3d36d39e6bd91807bddb5490912f0070659
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2025-03-19 23:12:20 -0600

    more CLI tools i use

--- dot_config/aconfmgr/20-shell.sh
+++ dot_config/aconfmgr/20-shell.sh
@@ -17,8 +17,14 @@ AddPackage sudo # Give certain users the ability to run some commands as root
 AddPackage tmux # Terminal multiplexer
 
 # Some additional utilities that I'm enjoying
+AddPackage bat # Cat clone with syntax highlighting and git integration
 AddPackage dust # A more intuitive version of du in rust
+AddPackage entr # Run arbitrary commands when files change
 AddPackage eza # A modern replacement for ls (community fork of exa)
+AddPackage github-cli # The GitHub CLI
 AddPackage hyfetch # Neofetch with LGBTQ+ pride flags!
+AddPackage inferno # A Rust port of FlameGraph
+AddPackage perf # Linux kernel performance auditing tool
 AddPackage rust-script # Run Rust files and expressions as scripts without any setup or compilation
+AddPackage tokei # A blazingly fast CLOC (Count Lines Of Code) program
 AddPackage tree # A directory listing program displaying a depth indented list of files
